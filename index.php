49d94f3a932b128d1972550004c182c4d190cf05
hanhlet1 <hanhlt20021996@gmail.com>
2018-11-07 15:36:56 +0700

    success calculator

--- index.php
+++ index.php
@@ -11,34 +11,34 @@
 </head>
 <body>
 <div class="container">
-<?php
-	if(isset($_GET['display'])){
-		echo $_GET['display'];
-	}else{
-		echo '';
-	}
-?>
-	<form action="" method="get">
+	<form action="" method="post">
 		<h2>Calculator basic</h2>
-		<input type="number" name="numA">
+		<input type="number" name="numA" value='<?php echo isset($_POST['numA'])? $_POST['numA'] : false ?>'>
 		<select name="oprator">
 			<option value="add" name="add">+</option>
 			<option value="minus" name="minus">-</option>
 			<option value="multiplication" name="multiplication">*</option>
 			<option value="division" name="division">/</option>
 		</select>
-		<input type="number" name="numB">
+		<input type="number" name="numB" value='<?php echo isset($_POST['numB'])? $_POST['numB'] : false ?>'>
 		<br>
 		<input type="submit" value="result">
 		<br>
 		<p class="result">
 			<?php
+//                $exprssions = ['+', '-', 'x', ':'];
+//
+//                + , - , *, /
+//                $arr = explode('+', '1+2');
+//
+//                $arr = [1, 2];
+//                if($express == '+')
 
-				$numA=isset($_GET['numA'])?$_GET['numA']:'';
-				$numB=isset($_GET['numB'])?$_GET['numB']:'';
-				$oprator=isset($_GET['oprator'])?$_GET['oprator']:'';
+
+				$numA=isset($_POST['numA'])?$_POST['numA']:'';
+				$numB=isset($_POST['numB'])?$_POST['numB']:'';
+				$oprator=isset($_POST['oprator'])?$_POST['oprator']:'';
 				if($numA!=='' && $numB !=='' && $oprator!==''){
-					echo "vao if";
 					if($oprator==='add'){
 						echo $numA+$numB;
 					}else if($oprator==='minus'){
@@ -52,15 +52,22 @@
 			?>
 		</p>
 	</form>
-	
+
+    <?php
+        $bt=(isset($_GET['bieuThuc']))?$_GET['bieuThuc']:'';
+        $result=(isset($_GET['result']))?$_GET['result']:'';
+        $result = eval('return '.$bt.';');
+    ?>
+
 
 
 
 	<fieldset id="container">
-		<form name="calculator" method="get" action="./index.php">
-			<input id="display" type="text" name="display" value="hanh" readonly>
+		<form name="calculator" method="GET" action="./index.php">
+			<input id="bieuThuc" class="display" type="text" name="bieuThuc" value='<?php echo $bt ?>' readonly>
+            <input id="result" class="display" type="text" name="result" value="<?php echo $result ?>" readonly>
 
-			<input class="button digits" type="button" value="7" >
+			<input class="button digits" type="button" value="7">
 			<input class="button digits" type="button" value="8">
 			<input class="button digits" type="button" value="9">
 			<input class="button mathButtons" type="button" value="+" >
@@ -73,14 +80,41 @@
 			<input class="button digits" type="button" value="1" >
 			<input class="button digits" type="button" value="2">
 			<input class="button digits" type="button" value="3" >
-			<input class="button mathButtons" type="button" value="x">
+			<input class="button mathButtons" type="button" value="*">
 			<br>
 			<input id="clearButton" class="button" type="button" value="C" >
 			<input class="button digits" type="button" value="0" >
-			<input class="button mathButtons" type="submit" value="=">
+			<input class="mathButtons result" type="submit" value="=">
 			<input class="button mathButtons" type="button" value="/" >
 		</form>
 	</fieldset>
 </div>
+
+<script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+
+<script>
+    $(document).ready(function(){
+
+       var input='';
+
+       $('#clearButton').on('click',function(){
+           $('#bieuThuc').val('');
+           $('#result').val('');
+       });
+
+       $('input.button').on('click',function(){
+           var value=$(this)[0].value;
+           if(value==='C'){
+               $('#bieuThuc').val('');
+           }else {
+               input = input + value;
+               $('#bieuThuc').val(input);
+           }
+       });
+    });
+</script>
 </body>
 </html>
